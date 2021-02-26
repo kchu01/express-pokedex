2ea5f9c73b9907f009f6d09fd36579ae571e01e2
kchu01 <chukathy01@gmail.com>
2021-02-26 13:14:17 -0800

    Add Models, DB, and Pikachu

--- server.js
+++ server.js
@@ -1,7 +1,37 @@
 /* Required Modules and Variables */
+const express = require('express')
+const ejsLayouts = require('express-ejs-layouts')
+const rowdy = require('rowdy-logger')
+const axios = require('axios')
 
+const app = express()
+const rowdyResults = rowdy.begin(app)
+const PORT = process.env.PORT || 3000
+
+// Middleware and config
+app.set('view engine', 'ejs')
 
 /* Controllers */
 
 
 /* Routes */
+app.get('/', async (req, res) => {
+    try {
+        const pokeURL = 'https://pokeapi.co/api/v2/pokemon?limit=150'
+        const responce = await axios.get(pokeURL)
+        const pokemons = responce.data.results
+
+        res.render('index', { pokemons: pokemons })
+    } catch (error) {
+        console.log(error)
+        res.render('index', { pokemons: [] })
+    }
+})
+
+// app.post('/', (req, res) => {
+//     res.json('https://pokeapi.co/api/v2/pokemon?limit=150')
+// })
+
+app.listen(PORT, () => {
+    rowdyResults.print()
+})
